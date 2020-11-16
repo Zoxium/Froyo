da1dec36214c3d5d51f9afcf512af370460ba26c
Zoxium <31391261+Zoxium@users.noreply.github.com>
2020-11-16 16:01:19 -0500

    Yeah. Main.js.
    Nice.

---
+++ main.js
@@ -0,0 +1,8 @@
+
+// Add code here. Make sure you know what you're doing before changing stuff here.
+
+function printthisdumbtextoutpleasecomputerdothisformeelseiwilldie() {
+  console.log("Hey. Good computer.")
+}
+
+setInterval(printthisdumbtextoutpleasecomputerdothisformeelseiwilldie, 1000)
